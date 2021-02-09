b776e30a8046bdf63a07cdf5dee246a9377e8a25
Ashley U <ashley.umetsu@gmail.com>
2021-02-08 21:52:18 -0600

    Added moveRight and moveLeft functions and fixed bugs

--- js/Token.js
+++ js/Token.js
@@ -34,22 +34,40 @@ class Token {
 	}
 
 	/** 
-	 * Moves html token one column to the left.
-	 */
-	 moveLeft() {
-	 	if (this.columnLocation > 0) {
-	 		this.htmlToken.style.left = this.offsetLeft - 76;
-	 		this.columnLocation -= 1;
-	 	}
-	 }
-	 /** 
-	 * Moves html token one column to right.
-	 */
+     * Moves html token one column to left.
+     */
+	  moveLeft() {
+	    if (this.columnLocation > 0) {
+	      this.htmlToken.style.left = `${this.offsetLeft - 76}px`;
+	      this.columnLocation -= 1;
+	    }
+	  }
 
-	 moveRight(columns) {
-	 	if (this.columnLocation < columns - 1) {
-	 		this.htmlToken.style.left = this.offsetLeft + 76;
-	 		this.columnLocation += 1;
-	 	}
+	  moveRight(columns) {
+	    if (this.columnLocation < columns - 1) {
+	      this.htmlToken.style.left = `${this.offsetLeft + 76}px`;
+	      this.columnLocation += 1;
+	    }
+	  }
+    
+    
+    /** 
+     * Drops html token into targeted board space.
+     * @param   {Object}    Targeted space for dropped token.
+     * @param   {function}  The reset function to call after the drop animation has completed.
+     */
+	drop(target, reset) {
+        this.dropped = true;
+        
+        $(this.htmlToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
 	}
 }
+
+
+
+
+
+
+
